2eef6067a5b0d5b658772c680770c2509b31aebc
Nikita-1414 <officialnikita14@gmail.com>
2025-03-12 00:11:41 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import EventsSection from './components/EventsSection';
+import TeamMembersSection from './components/TeamMembersSection';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App min-h-screen flex flex-col bg-gradient-cyberpunk">
+        <Navbar />
+        
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={
+              <>
+                <EventsSection />
+                <TeamMembersSection />
+              </>
+            } />
+            <Route path="/events" element={<EventsSection />} />
+            <Route path="/team" element={<TeamMembersSection />} />
+            {/* Add more routes as needed */}
+          </Routes>
+        </main>
+        
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
